0bc1882abd704579649d3231ad63126a98b94297
Dawid Jasiński <ambas90@gmail.com>
2024-02-10 20:19:40 +0100

    Update Cast.jsx

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,3 +1,26 @@
+import { useParams } from 'react-router-dom';
+import useFilms from 'hooks/useFilms';
+
 export default function Cast() {
-  return <div>Cast</div>;
+  const { movieId } = useParams();
+  const { filmData } = useFilms(`movie/${movieId}/credits`);
+
+  console.log(filmData);
+  console.log(filmData.cast);
+  return (
+    <div>
+      <ul>
+        {filmData.cast?.map(actor => (
+          <li key={actor.id}>
+            <img
+              src={`https://image.tmdb.org/t/p/w400/${actor.profile_path}`}
+              alt=""
+            />
+            <h3>{actor.name}</h3>
+            <p>character: {actor.character}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
