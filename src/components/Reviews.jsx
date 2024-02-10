805a5e8dade0abd4eb4d6c92693781d38c945410
Dawid Jasiński <ambas90@gmail.com>
2024-02-10 20:30:03 +0100

    Update Reviews.jsx

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,3 +1,22 @@
+import { useParams } from 'react-router-dom';
+import useFilms from 'hooks/useFilms';
+
 export default function Reviews() {
-  return <div>Reviews</div>;
+  const { movieId } = useParams();
+  const { filmData } = useFilms(`movie/${movieId}/reviews`);
+
+  console.log(filmData);
+  return (
+    <div>
+      {filmData.total_results < 1 && <p>no reviews</p>}
+      <ul>
+        {filmData.results?.map(review => (
+          <li key={review.id}>
+            <h3>{review.author}</h3>
+            <p>{review.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
